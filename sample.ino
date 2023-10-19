46b3dca851a32ae4521143aa2983f638ecef322e
rampriyasrnmc <148441171+rampriyasrnmc@users.noreply.github.com>
2023-10-19 15:04:47 +0530

    Add files via upload

---
+++ sample.ino
@@ -0,0 +1,13 @@
+void setup() {
+ pinMode(13,OUTPUT); // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(13,HIGH);
+  delay(2000);
+  digitalWrite(13,LOW);
+  delay(5000);
+  // put your main code here, to run repeatedly:
+
+}
